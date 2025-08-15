57eea4411f9ab25a050b4f6d4d0f1fe301e165dd
Trang <ntttrang9090@gmail.com>
2025-08-15 21:56:47 +0700

    add mobile view port test case

--- src/test/java/browsers/ChromeTest.java
+++ src/test/java/browsers/ChromeTest.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChromeTest {
 
     @Test
@@ -25,6 +28,24 @@ public class ChromeTest {
     public void headlessModeTest(){
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--headless");
+
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(),"Selenium");
+        driver.quit();
+    }
+
+    //test case kiểm tra Responsive trên web
+    @Test
+    void mobileViewPortraitTest(){
+        Map<String, Object> deviceMetrics = new HashMap<>();
+        deviceMetrics.put("width", 344);
+        deviceMetrics.put("height", 882);
+        Map<String, Object> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceMetrics", deviceMetrics);
+
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
 
         WebDriver driver = new ChromeDriver(chromeOptions);
         driver.get("https://www.selenium.dev/");
