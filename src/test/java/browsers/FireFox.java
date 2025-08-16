b55376e4172f5c968af43b4d9ee57fd5ac7023b9
Trang <ntttrang9090@gmail.com>
2025-08-16 13:42:29 +0700

    add FireFox test

---
+++ src/test/java/browsers/FireFox.java
@@ -0,0 +1,28 @@
+package browsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FireFox {
+    @Test
+    void openBrowserWithDefaultMode() {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(), "Selenium");
+        driver.quit();
+    }
+
+    @Test
+    void openBrowserWithHeadlessMode() {
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--headless");
+
+        WebDriver driver = new FirefoxDriver(firefoxOptions);
+        driver.get("https://www.selenium.dev/");
+        Assert.assertEquals(driver.getTitle(), "Selenium");
+        driver.quit();
+    }
+}
